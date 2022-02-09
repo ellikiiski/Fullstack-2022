2cf0a0a7234ecb44b02d51be2d67f19167f225da
ellikiiski <kiiski.elli@gmail.com>
2022-02-10 00:19:55 +0200

    klikkausversio toimii hähä

--- osa-2/maatietoa/src/App.js
+++ osa-2/maatietoa/src/App.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Country = ({ country }) => {
+const Country = ({ country, showFunc }) => {
   return (
     <div>
-      {country.name.common}
+      {country.name.common} <input className={country.name.common} value="show" type="button" onClick={showFunc}/>
     </div>
   )
 }
 
 const CountryInfo = ({ country }) => {
-  console.log(country)
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -68,15 +67,19 @@ const Flag = ({ country }) => {
   )
 }
 
-const Info = ({ countries }) => {
-  if (countries.length >= 10) {
+const Info = ({ countries, chosen, showFunc }) => {
+  const listOfCountries = Array.from(countries.values())
+  if (chosen != "") {
+    return (<CountryInfo country={countries.get(chosen)} />)
+  }
+  if (listOfCountries.length === 1) {
+    return (<CountryInfo country={listOfCountries[0]} />)
+  }
+  if (listOfCountries.length >= 10) {
     return (<div>Too many matches, specify more</div>)
   }
-  if (countries.length > 1) {
-    return (<div>{countries.map(country => <Country key={country.name.common} country={country} />)}</div>)
-  }
-  if (countries.length === 1) {
-    return (<CountryInfo country={countries[0]} />)
+  if (listOfCountries.length > 1) {
+    return (<div>{listOfCountries.map(country => <Country key={country.name.common} country={country} showFunc={showFunc} />)}</div>)
   }
   return (<div>No matches</div>)
 }
@@ -85,26 +88,40 @@ const App = () => {
 
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
+  const [chosen, setChosen] = useState('')
 
   useEffect(() => {
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
-        setCountries(response.data)
-      })
+      setCountries(mapCountries(response.data))
+    })
   }, [])
 
+  const mapCountries = (list) => {
+    const mappa = new Map()
+    list.forEach(country => mappa.set(country.name.common, country))
+    return (mappa)
+  }
+
+  const showCountry = (event) => {
+    event.preventDefault()
+    setChosen(event.target.className)
+  }
+
   const handleFiltering = (event) => {
+    if (event.target.value === "") {
+      setChosen('')
+    }
     setFilter(event.target.value)
   }
 
-  //const names = countries.map(country => country.name.common)
-  const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
+  const filteredCountries = new Map([...countries].filter(([name, country]) => name.toLowerCase().includes(filter.toLowerCase())))
 
   return (
     <div>
       find countries: <input value={filter} onChange={handleFiltering}></input>
-      <Info countries={filteredCountries} />
+      <Info countries={filteredCountries} chosen={chosen} showFunc={showCountry} />
     </div>
   )
 
