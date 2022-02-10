23f09776bda9490389321e2f0a4de8d0f8e5b38d
ellikiiski <kiiski.elli@gmail.com>
2022-02-10 10:04:53 +0200

    vähän muutin järkkää

--- osa-2/maatietoa/src/Info.js
+++ osa-2/maatietoa/src/Info.js
@@ -12,88 +12,88 @@ import React from 'react'
 //
 // Toivottavasti tämän toiminnallisuuden tekemiseen saa vähän
 // parempia eväitä kurssin edetessä. :D
+  
+const Population = ({ country }) => {
+  return (
+    <div>
+      <b>Population </b> {country.population}
+    </div>
+  )
+}
+  
+const Capital = ({ country }) => {
+  return (
+    <div>
+      <b>Capital</b> {country.capital}
+    </div>
+  )
+}
+  
+const Timezones = ({ country }) => {
+  const zones = country.timezones
+  return (
+    <div>
+      <b>{zones.length} timezone(s)</b> {zones.join(", ")}
+    </div>
+  )
+}
+  
+const Languages = ({ country }) => {
+  const languages = Object.entries(country.languages).map(language => language[1])
+  return (
+    <div>
+      <b>Languages</b>
+      <ul>
+        {languages.map(language => <li key={language}>{language}</li>)}
+      </ul>
+    </div>
+  )
+}
+  
+const Flag = ({ country }) => {
+  return (
+    <div>
+      <img src={country.flags.svg} height="100"></img>
+    </div>
+  )
+}
 
 const Country = ({ country, showFunc }) => {
-    return (
-      <div>
-        {country.name.common} <input className={country.name.common} value="show" type="button" onClick={showFunc}/>
-      </div>
-    )
-  }
+  return (
+    <div>
+      {country.name.common} <input className={country.name.common} value="show" type="button" onClick={showFunc}/>
+    </div>
+  )
+}
   
-  const CountryInfo = ({ country }) => {
-    return (
-      <div>
-        <h2>{country.name.common}</h2>
-        <Capital country={country} />
-        <Population country={country} />
-        <Timezones country={country} />
-        <Languages country={country} />
-        <Flag country={country} />
-      </div>
-    )
-  }
+const CountryInfo = ({ country }) => {
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <Capital country={country} />
+      <Population country={country} />
+      <Timezones country={country} />
+      <Languages country={country} />
+      <Flag country={country} />
+    </div>
+  )
+}
   
-  const Population = ({ country }) => {
-    return (
-      <div>
-        <b>Population </b> {country.population}
-      </div>
-    )
+const Info = ({ countries, chosen, showFunc }) => {
+  const listOfCountries = Array.from(countries.values())
+  if (chosen != "") {
+    return (<CountryInfo country={countries.get(chosen)} />)
   }
-  
-  const Capital = ({ country }) => {
-    return (
-      <div>
-        <b>Capital</b> {country.capital}
-      </div>
-    )
+  if (listOfCountries.length === 1) {
+    return (<CountryInfo country={listOfCountries[0]} />)
   }
-  
-  const Timezones = ({ country }) => {
-    const zones = country.timezones
-    return (
-      <div>
-        <b>{zones.length} timezone(s)</b> {zones.join(", ")}
-      </div>
-    )
+  if (listOfCountries.length >= 10) {
+    return (<div>Too many matches, specify more</div>)
   }
-  
-  const Languages = ({ country }) => {
-    const languages = Object.entries(country.languages).map(language => language[1])
-    return (
-      <div>
-        <b>Languages</b>
-        <ul>
-          {languages.map(language => <li key={language}>{language}</li>)}
-        </ul>
-      </div>
-    )
+  if (listOfCountries.length > 1) {
+    return (<div>{listOfCountries.map(country => <Country key={country.name.common} country={country} showFunc={showFunc} />)}</div>)
   }
-  
-  const Flag = ({ country }) => {
-    return (
-      <div>
-        <img src={country.flags.svg} height="100"></img>
-      </div>
-    )
-  }
-  
-  const Info = ({ countries, chosen, showFunc }) => {
-    const listOfCountries = Array.from(countries.values())
-    if (chosen != "") {
-      return (<CountryInfo country={countries.get(chosen)} />)
-    }
-    if (listOfCountries.length === 1) {
-      return (<CountryInfo country={listOfCountries[0]} />)
-    }
-    if (listOfCountries.length >= 10) {
-      return (<div>Too many matches, specify more</div>)
-    }
-    if (listOfCountries.length > 1) {
-      return (<div>{listOfCountries.map(country => <Country key={country.name.common} country={country} showFunc={showFunc} />)}</div>)
-    }
-    return (<div>No matches</div>)
-  }
+  return (<div>No matches</div>)
+}
 
-  export default Info
+export default Info
