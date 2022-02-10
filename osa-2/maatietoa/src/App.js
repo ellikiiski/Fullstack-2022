9d65b58e500cdc4c70051791d142b05d191b697f
ellikiiski <kiiski.elli@gmail.com>
2022-02-10 09:46:19 +0200

    refaktorointia ja disclaimeröintiä

--- osa-2/maatietoa/src/App.js
+++ osa-2/maatietoa/src/App.js
@@ -1,87 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Info from './Info'
 
-const Country = ({ country, showFunc }) => {
+// DISCLAIMER
+//
+// Otin maahakuun huomioon myös corner caset (esim. Sudan),
+// vaikka pitikin käyttää aika outoja keinoja.
+// Maainfo lähtee pois näkyvistä kun filtterikentän tyhjentää kokonaan.
+//
+// Ja tämä siis siksi, että mielestäni tehtävä 2.13 oli idioottimainen,
+// koska napit eivät tuoneet mitään iloa sivun toimintaan
+// (ymmärrän kyllä pedagokisen tarkoituksen, mutta tyhmää silti).
+//
+// Toivottavasti tämän toiminnallisuuden tekemiseen saa vähän
+// parempia eväitä kurssin edetessä. :D
+
+const Filter = ({ value, onChange }) => {
   return (
     <div>
-      {country.name.common} <input className={country.name.common} value="show" type="button" onClick={showFunc}/>
+      filter: <input value={value} onChange={onChange}></input>
     </div>
   )
-}
-
-const CountryInfo = ({ country }) => {
-  return (
-    <div>
-      <h2>{country.name.common}</h2>
-      <Capital country={country} />
-      <Population country={country} />
-      <Timezones country={country} />
-      <Languages country={country} />
-      <Flag country={country} />
-    </div>
-  )
-}
-
-const Population = ({ country }) => {
-  return (
-    <div>
-      <b>Population </b> {country.population}
-    </div>
-  )
-}
-
-const Capital = ({ country }) => {
-  return (
-    <div>
-      <b>Capital</b> {country.capital}
-    </div>
-  )
-}
-
-const Timezones = ({ country }) => {
-  const zones = country.timezones
-  return (
-    <div>
-      <b>{zones.length} timezone(s)</b> {zones.join(", ")}
-    </div>
-  )
-}
-
-const Languages = ({ country }) => {
-  const languages = Object.entries(country.languages).map(language => language[1])
-  return (
-    <div>
-      <b>Languages</b>
-      <ul>
-        {languages.map(language => <li key={language}>{language}</li>)}
-      </ul>
-    </div>
-  )
-}
-
-const Flag = ({ country }) => {
-  return (
-    <div>
-      <img src={country.flags.svg} height="100"></img>
-    </div>
-  )
-}
-
-const Info = ({ countries, chosen, showFunc }) => {
-  const listOfCountries = Array.from(countries.values())
-  if (chosen != "") {
-    return (<CountryInfo country={countries.get(chosen)} />)
-  }
-  if (listOfCountries.length === 1) {
-    return (<CountryInfo country={listOfCountries[0]} />)
-  }
-  if (listOfCountries.length >= 10) {
-    return (<div>Too many matches, specify more</div>)
-  }
-  if (listOfCountries.length > 1) {
-    return (<div>{listOfCountries.map(country => <Country key={country.name.common} country={country} showFunc={showFunc} />)}</div>)
-  }
-  return (<div>No matches</div>)
 }
 
 const App = () => {
@@ -89,6 +28,9 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
   const [chosen, setChosen] = useState('')
+
+  const filteredCountries = new Map([...countries].filter(([name, country]) =>
+    name.toLowerCase().includes(filter.toLowerCase())))
 
   useEffect(() => {
     axios
@@ -99,9 +41,9 @@ const App = () => {
   }, [])
 
   const mapCountries = (list) => {
-    const mappa = new Map()
-    list.forEach(country => mappa.set(country.name.common, country))
-    return (mappa)
+    const mapped = new Map()
+    list.forEach(country => mapped.set(country.name.common, country))
+    return (mapped)
   }
 
   const showCountry = (event) => {
@@ -110,21 +52,18 @@ const App = () => {
   }
 
   const handleFiltering = (event) => {
-    if (event.target.value === "") {
+    if (event.target.value === '') {
       setChosen('')
     }
     setFilter(event.target.value)
   }
 
-  const filteredCountries = new Map([...countries].filter(([name, country]) => name.toLowerCase().includes(filter.toLowerCase())))
-
   return (
     <div>
-      find countries: <input value={filter} onChange={handleFiltering}></input>
+      find countries: <Filter value={filter} onChange={handleFiltering} />
       <Info countries={filteredCountries} chosen={chosen} showFunc={showCountry} />
     </div>
   )
-
 }
 
 export default App
